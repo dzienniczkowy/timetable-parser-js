d786ebe420063ea6c26c893931ac078758003843
Dominik Korsa <dominik.korsa@gmail.com>
2019-05-30 17:40:56 +0200

    Add unordered lists

--- lib/TimetableList.ts
+++ lib/TimetableList.ts
@@ -10,6 +10,8 @@ export default class TimetableList {
   public getList(): Record<string, Record<string, string>[]> {
     if (this.getListType() === 'select') {
       return this.getSelectList();
+    } if (this.getListType() === 'unordered') {
+      return this.getUnorderedList();
     }
 
     throw new Error('List type not suported');
@@ -43,7 +45,47 @@ export default class TimetableList {
     nodes.forEach((node): void => {
       values.push({
         name: this.$(node).text(),
-        value: node.attribs.value,
+        value: this.$(node).attr('value'),
+      });
+    });
+
+    return values;
+  }
+
+  private getUnorderedList(): Record<string, Record<string, string>[]> {
+    let teachersQuery = 'ul:nth-of-type(2) a';
+    let roomsQuery = 'ul:nth-of-type(3) a';
+    if (this.$('h4').length === 1) {
+      teachersQuery = 'undefined';
+      roomsQuery = 'undefined';
+    } else if (this.$('h4:nth-of-type(2)').text() === 'Sale') {
+      teachersQuery = 'undefined';
+      roomsQuery = 'ul:nth-of-type(2) a';
+    }
+    return this.getTimetableUrlSubType(
+      'ul:first-of-type a',
+      teachersQuery,
+      roomsQuery,
+    );
+  }
+
+  private getTimetableUrlSubType(classQuery: string, teachersQuery: string, roomsQuery: string):
+  Record<string, Record<string, string>[]> {
+    return {
+      classes: this.getSubTypeValue(classQuery, 'o'),
+      teachers: this.getSubTypeValue(teachersQuery, 'n'),
+      rooms: this.getSubTypeValue(roomsQuery, 's'),
+    };
+  }
+
+  private getSubTypeValue(query: string, prefix: string): Record<string, string>[] {
+    const values: Record<string, string>[] = [];
+
+    const nodes = this.$(query).toArray();
+    nodes.forEach((node: CheerioElement): void => {
+      values.push({
+        name: this.$(node).text(),
+        value: this.$(node).attr('href').replace('.html', '').replace(`plany/${prefix}`, ''),
       });
     });
 
