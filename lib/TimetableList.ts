2ab06cfd0b242bf4fc096d10ef9173d87954a5b6
Dominik Korsa <dominik.korsa@gmail.com>
2019-05-30 17:15:45 +0200

    Add select list

--- lib/TimetableList.ts
+++ lib/TimetableList.ts
@@ -7,6 +7,14 @@ export default class TimetableList {
     this.$ = cheerio.load(html);
   }
 
+  public getList(): Record<string, Record<string, string>[]> {
+    if (this.getListType() === 'select') {
+      return this.getSelectList();
+    }
+
+    throw new Error('List type not suported');
+  }
+
   public getListType(): string {
     if (this.$('form[name=form]').length > 0) {
       return 'select';
@@ -17,5 +25,28 @@ export default class TimetableList {
     }
 
     return 'unordered';
+  }
+
+  private getSelectList(): Record<string, Record<string, string>[]> {
+    return {
+      classes: this.getSelectListValues('oddzialy'),
+      teachers: this.getSelectListValues('nauczyciele'),
+      rooms: this.getSelectListValues('sale'),
+    };
+  }
+
+  private getSelectListValues(name: string): Record<string, string>[] {
+    const nodes = this.$(`[name=${name}] option`).toArray();
+    nodes.shift();
+
+    const values: Record<string, string>[] = [];
+    nodes.forEach((node): void => {
+      values.push({
+        name: this.$(node).text(),
+        value: node.attribs.value,
+      });
+    });
+
+    return values;
   }
 }
